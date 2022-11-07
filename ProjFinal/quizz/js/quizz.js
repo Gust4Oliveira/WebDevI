504ec20614cf7bb01772502353e031cb657e9b17
Gust4Oliveira <gustavoolacerda@outlook.com.br>
2022-11-06 23:56:37 -0300

    Proj Final

---
+++ ProjFinal/quizz/js/quizz.js
@@ -0,0 +1,214 @@
+//this would be the object shape for storing the questions  
+ //you can change the questions to your own taste or even add more questions..
+ const questions = [
+   {
+        question: "Quantos países no mundo usam o voto eletrônico?",
+        optionA: "Apenas três em todo mundo.",
+        optionB: "Entre três e dez países.",
+        optionC: "De 10 à 20 países.",
+        optionD: "Mais de 20 países.",
+        correctOption: "optionD"
+    },
+    {
+        question: "Como seu voto vai da urna eletrônica até o TSE?",
+        optionA: "Via rede de internet privativa (ou VPN).",
+        optionB: "Por sinal de fumaça.",
+        optionC: "Via internet comum, ou aberta.",
+        optionD: "Resultados das urnas são impressos e transportados por motos, barcos ou carros diretamente até o TSE.",
+        correctOption: "optionA"
+    },
+    {
+        question: "O que acontece se eu não votar?",
+        optionA: "Nada.",
+        optionB: "Nada, mas só se eu tiver menos de 18 ou mais de 69 anos.",
+        optionC: "Perco automaticamente o título de eleitor e tenho que tirar outro.",
+        optionD: "Sou preso.",
+        correctOption: "optionB"
+    },
+    {
+        question: "Quem tem o maior salário: presidente, senador ou deputado?",
+        optionA: "O presidente.",
+        optionB: "O senador.",
+        optionC: "O deputado.",
+        optionD: "O governador.",
+        correctOption: "optionD"
+    },
+    {
+        question: "Quantos senadores e deputados federais serão eleitos esse ano?",
+        optionA: "54 senadores e 438 deputados federais.",
+        optionB: "81 senadores e 342 deputados federais.",
+        optionC: "27 senadores e 513 deputados federais.",
+        optionD: "49 senadores e 308 deputados federais.",
+        correctOption: "optionC"
+    },
+    {
+        question: "Votos nulos e brancos podem influenciar uma eleição?",
+        optionA: "Diretamente, não. Indiretamente, sim.",
+        optionB: "Sim, mas depende da quantidade.",
+        optionC: "Sim, sempre.",
+        optionD: "Não, nunca.",
+        correctOption: "optionA"
+    },
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+//function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 5) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 5) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 2) {
+        remark = "Péssimo, continue estudando."
+        remarkColor = "red"
+    }
+    else if (playerScore > 2 && playerScore < 4) {
+        remark = "Razoável, você consegue melhorar."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 4) {
+        remark = "Ótimo, continue assim."
+        remarkColor = "green"
+    }
+
+    const playerGrade = (playerScore / 6) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
